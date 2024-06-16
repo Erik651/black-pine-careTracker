447abc8c01f6a7584bc7073e8eeb9452e00ec004
Erik Silcox <eriksilcox651@gmail.com>
2024-06-15 20:03:14 -0500

    installed Youtube. added example to Links.jsx

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -1,10 +1,58 @@
-function Links() {
-  return (
-    <div>Links
-      <p></p>
-    </div>
-    
-  )
-}
+import React from 'react';
+import YouTube from 'react-youtube';
+
+const Links = () => {
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const onReady = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  return <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={onReady} />;
+};
 
 export default Links;
+
+
+// import React from 'react';
+// import YouTube from 'react-youtube';
+
+
+//   Links() {
+//     const opts = {
+//       height: '390',
+//       width: '640',
+//       playerVars: {
+//         // https://developers.google.com/youtube/player_parameters
+//         autoplay: 1,
+//       },
+//     };
+
+//     return <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />;
+//   };
+
+//   export default Links;
+  // _onReady(event) {
+  //   // access to player in all event handlers via event.target
+  //   event.target.pauseVideo();
+  // }
+
+
+
+
+// function Links() {
+//   return (
+//     <div>Links
+//       <p></p>
+//     </div>
+    
+//   )
+// }
+
